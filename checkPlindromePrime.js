f365b4ed406bc952500cd1f07aa15aba5a7de4f9
yogesh logade <yogeshlogade66@gmail.com>
2022-12-02 13:36:57 +0530

    done

--- checkPlindromePrime.js
+++ checkPlindromePrime.js
@@ -1,4 +1,5 @@
 //Checking prime
+let count = 0;
 let num = 7;
 for(let i = 1; i<=num; i++){
   if(num%i==0){
@@ -13,6 +14,7 @@ else{
 }
 
 //Checking palindrome
+let bag = "";
 let str = "mmaamm";
 for(let i = str.length-1; i>=0 ; i--){
     bag += str[i];
